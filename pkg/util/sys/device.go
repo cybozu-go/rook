c7d9f2f05e446ff30e869ec10bbc2e9902c8a188
Hiroshi Muraoka <h.muraoka714@gmail.com>
2020-03-10 09:16:08 +0000

    Fix to run PopulateDeviceUdevInfo

--- pkg/util/sys/device.go
+++ pkg/util/sys/device.go
@@ -199,7 +199,7 @@ func GetDeviceProperties(device string, executor exec.Executor) (map[string]stri
 // GetDevicePropertiesFromPath gets a device property from a path
 func GetDevicePropertiesFromPath(devicePath string, executor exec.Executor) (map[string]string, error) {
 	output, err := executor.ExecuteCommandWithOutput("lsblk", devicePath,
-		"--bytes", "--nodeps", "--pairs", "--output", "SIZE,ROTA,RO,TYPE,PKNAME,NAME")
+		"--bytes", "--nodeps", "--pairs", "--path", "--output", "SIZE,ROTA,RO,TYPE,PKNAME,NAME")
 	if err != nil {
 		// The "not a block device" error also returns code 32 so the ExitStatus() check hides this error
 		if strings.Contains(output, "not a block device") {
@@ -367,6 +367,9 @@ func isDeviceAvailable(executor exec.Executor, device string) (bool, string, err
 func inventoryDevice(executor exec.Executor, dev string) (CephVolumeInventory, error) {
 	var CVInventory CephVolumeInventory
 	device := path.Join("/dev", dev)
+	if strings.Contains(dev, "/") {
+		device = dev
+	}
 
 	args := []string{"inventory", "--format", "json", device}
 	inventory, err := executor.ExecuteCommandWithOutput("ceph-volume", args...)
