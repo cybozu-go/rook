c6d7ccec234d3508c4935da2d40e0fb7269e4a80
morimoto-cybozu <kenji_morimoto@cybozu.co.jp>
2020-03-10 12:00:02 +0000

    Get KNAME of device.
    Signed-off-by: morimoto-cybozu <kenji_morimoto@cybozu.co.jp>

--- pkg/clusterd/disk.go
+++ pkg/clusterd/disk.go
@@ -150,6 +150,9 @@ func PopulateDeviceInfo(d string, executor exec.Executor) (*sys.LocalDisk, error
 	if val, ok := diskProps["NAME"]; ok {
 		disk.RealPath = val
 	}
+	if val, ok := diskProps["KNAME"]; ok {
+		disk.KernelName = path.Base(val)
+	}
 
 	return disk, nil
 }
